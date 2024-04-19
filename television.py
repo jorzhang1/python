23494beb59ade21d2a00d6beb2f2d39ee9491278
Jordan Zhang <jordanzhang@unomaha.edu>
2024-04-19 17:11:27 -0500

    Edited Code

--- television.py
+++ television.py
@@ -5,28 +5,64 @@ class Television:
     MAX_CHANNEL = 3
 
     def __init__(self):
-        __status = False
-        __muted = False
-        __volume = Television.MIN_VOLUME
-        __channel = Television.MIN_CHANNEL
+        self.__status = False
+        self.__muted = False
+        self.__volume = Television.MIN_VOLUME
+        self.__ogVolume = 0
+        self.__channel = Television.MIN_CHANNEL
 
     def power(self):
-        pass
+        if not self.__status:
+            self.__status = True
+        else:
+            self.__status = False
 
     def mute(self):
-        pass
+        if self.__status:
+            if not self.__muted:
+                self.__ogVolume = self.__volume
+                self.__volume = 0
+                self.__muted = True
+            else:
+                self.__volume = self.__ogVolume
+                self.__ogVolume = 0
+                self.__muted = False
 
     def channel_up(self):
-        pass
+        if self.__status:
+            if self.__channel == Television.MAX_CHANNEL:
+                self.__channel = Television.MIN_CHANNEL
+            else:
+                self.__channel += 1
 
     def channel_down(self):
-        pass
+        if self.__status:
+            if self.__channel == Television.MIN_CHANNEL:
+                self.__channel = Television.MAX_CHANNEL
+            else:
+                self.__channel -= 1
 
     def volume_up(self):
-        pass
+        if self.__status:
+            if self.__muted:
+                self.__volume = self.__ogVolume
+                self.__muted = False
+                if self.__volume < Television.MAX_VOLUME:
+                    self.__volume += 1
+            else:
+                if self.__volume < Television.MAX_VOLUME:
+                    self.__volume += 1
 
     def volume_down(self):
-        pass
+        if self.__status:
+            if self.__muted:
+                self.__volume = self.__ogVolume
+                self.__muted = False
+                if self.__volume > Television.MIN_VOLUME:
+                    self.__volume -= 1
+            else:
+                if self.__volume > Television.MIN_VOLUME:
+                    self.__volume -= 1
 
     def __str__(self):
-        pass
+        return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}.'
