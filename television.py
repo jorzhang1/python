cfec740cbb4b095fcbf9bc16471ec1cffb36e879
Jordan Zhang <jordanzhang@unomaha.edu>
2024-04-19 14:52:52 -0500

    Edited Code

--- television.py
+++ television.py
@@ -1,5 +1,32 @@
 class Television:
-    def __init__(self, animal):
-        self.animal = animal
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
 
-print('hi')
+    def __init__(self):
+        __status = False
+        __muted = False
+        __volume = Television.MIN_VOLUME
+        __channel = Television.MIN_CHANNEL
+
+    def power(self):
+        pass
+
+    def mute(self):
+        pass
+
+    def channel_up(self):
+        pass
+
+    def channel_down(self):
+        pass
+
+    def volume_up(self):
+        pass
+
+    def volume_down(self):
+        pass
+
+    def __str__(self):
+        pass
